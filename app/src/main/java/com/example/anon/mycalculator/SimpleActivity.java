aa5431ad55b0b64ffa8e0d8358ad0c995b3dbef6
StrollingOx <StrollingOx@github.com>
2019-05-27 23:37:35 +0200

    Simple calculator is working complete, waiting for bug feedback

--- app/src/main/java/com/example/anon/mycalculator/SimpleActivity.java
+++ app/src/main/java/com/example/anon/mycalculator/SimpleActivity.java
@@ -2,6 +2,7 @@ package com.example.anon.mycalculator;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.regex.Pattern;
 
 public class SimpleActivity extends AppCompatActivity
@@ -28,28 +31,57 @@ public class SimpleActivity extends AppCompatActivity
         updateScreen();
 
     }
-
     protected void initLayout()
     {
         screen = (TextView)findViewById(R.id.textView);
     }
-
     private void updateScreen()
     {
         screen.setText(display);
     }
 
+
+    private boolean isNumbersLengthCorrect()
+    {
+        if(currentOperator.equals("") && display.length()>13)
+            return false;
+
+        if(!currentOperator.equals(""))
+        {
+            String [] numbers = display.split(Pattern.quote(currentOperator));
+
+            if(numbers.length>1)
+            {
+                if (numbers[1].length() > 13) return false;
+                else return true;
+            }
+            else
+                return true;
+
+        }
+
+        return true;
+    }
     public void onClickNumber(View v)
     {
-        if(result != "")
+        if(!result.equals(""))
         {
             clear();
             updateScreen();
         }
+        if(!isNumbersLengthCorrect())
+        {
+           return;
+        }
+
         Button button = (Button) v;
         display += button.getText();
         updateScreen();
+
+
+
     }
+
     private boolean isOperator(char op)
     {
         switch(op)
@@ -63,13 +95,13 @@ public class SimpleActivity extends AppCompatActivity
     }
     public void onClickOperator(View v)
     {
-        //TODO: changing operation causes display errors
         if(display.equals("")) return;
 
         Button button = (Button) v;
 
         if(!result.equals(""))
         {
+            //First number will be previous equation's result
             String _display = result;
             clear();
             display = _display;
@@ -77,10 +109,10 @@ public class SimpleActivity extends AppCompatActivity
 
         if(!currentOperator.equals(""))
         {
-            Log.d("CalcX", ""+display.charAt(display.length()-1));
-            if(isOperator(display.charAt(display.length()-1)))
+            if(isOperator(display.charAt(display.length()-2)))
             {
-                display = display.replace(display.charAt(display.length() - 1), button.getText().charAt(0));
+                display = display.replace(display.charAt(display.length() - 2), button.getText().charAt(0));
+                currentOperator = button.getText().toString();
                 updateScreen();
                 return;
             }else
@@ -92,34 +124,110 @@ public class SimpleActivity extends AppCompatActivity
             currentOperator = button.getText().toString();
         }
 
-        display += button.getText();
+        display += "\n" + button.getText() + "\n"; //TODO: If app fucks up, go to getResult() and delete replaceAll(...)
         currentOperator = button.getText().toString();
         updateScreen();
     }
+
     public void onClickDot(View v)
     {
-        //Button button = (Button) v;
-        //TODO: 0.6.6.6.6.6 error
-        if(display.equals(""))
-            display = "0.";
-        else if(  display.charAt(display.length()-1) == '+'
-                || display.charAt(display.length()-1) == '-'
-                || display.charAt(display.length()-1) == 'x'
-                || display.charAt(display.length()-1) == '÷')
+        if(!result.equals(""))
+        {
+            //First number will be previous equation's result
+            String _display = result;
+            clear();
+            display = _display;
+        }
+
+        if(display.equals("")
+                || (!currentOperator.equals("")
+                    && display.charAt(display.length()-2) == currentOperator.charAt(0))) //-2 instead of -1 beacuse of the '\n'
             display+="0.";
-        else if(!display.equals("")
-                && !isOperator(display.charAt(display.length()-1))
-                && display.charAt(display.length()-1) != '.')
-            display += ".";
-        else if(display.charAt(display.length()-1) == '.')
-            return;
+
+        if(canInsertDot())
+        {
+            display+=".";
+        }
+
         updateScreen();
+    }
+    private boolean canInsertDot()
+    {
+        if(!isNumbersLengthCorrect())
+            return false;
+
+        if(currentOperator.equals("") && display.contains("."))
+            return false;
+
+
+        if(!currentOperator.equals(""))
+        {
+            String [] numbers = display.split(Pattern.quote(currentOperator));
+
+            if(numbers.length>1)
+                if (numbers[1].contains("."))
+                    return false;
+
+        }
+
+        return true;
     }
 
     public void onClickSignChange (View v)
     {
-        //Button button = (Button) v;
+        if(!result.equals(""))
+        {
+            return;
+        }
+
+        if(display.equals(""))
+        {
+            return;
+        }
+        else
+        {
+            if(display.charAt(0) == '-')
+            {
+                display = display.substring(1);
+            }
+            else
+                display = "-" + display;
+
+            updateScreen();
+        }
+
     }
+
+    public void onClickDelete(View v)
+    {
+        if(!display.equals("") && display.charAt(display.length()-1) == '\n')
+        {
+            display = deleteLastChar(display);
+            display = deleteLastChar(display);
+            display = deleteLastChar(display);
+            currentOperator = "";
+
+        }
+        else if(!display.equals("") && screen.getText().toString().contains("="))
+        {
+            return;
+        }
+        else
+        {
+            if(!display.equals(""))
+                display = deleteLastChar(display);
+        }
+        updateScreen();
+
+    }
+
+    public String deleteLastChar(String str) {
+        if (str != null && str.length() > 0) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
+
     private void clear()
     {
         display = "";
@@ -131,28 +239,26 @@ public class SimpleActivity extends AppCompatActivity
         clear();
         updateScreen();
     }
-    private double operate (String aa, String bb, String op)
+    private double operate (String aa, String bb,@NonNull String op)
     {
-        //TODO: 10.1 * 6  = 60.59999 error EDIT: kinda fixed....
         double a = Double.valueOf(aa);
-        double b = Double.valueOf(bb);
         BigDecimal BDa = new BigDecimal(aa);
+
+        double b = Double.valueOf(bb);
         BigDecimal BDb = new BigDecimal(bb);
-        System.out.println(a);
-        System.out.println(b);
+
         switch (op)
         {
             case "+":
-                return a+b;
+                return Double.valueOf(BDa.add(BDb).toString());
             case "-":
-                return a-b;
+                return Double.valueOf(BDa.subtract(BDb).toString());
             case "x":
-                System.out.println((float)a*b);
                 return Double.valueOf(BDa.multiply(BDb).toString());
             case "÷":
                 try
                 {
-                    return a/b; //Should i use BigDecimal here too?
+                    return Double.valueOf((BDa.divide(BDb, new MathContext(8, RoundingMode.HALF_UP))).toString());
                 }catch(Exception e)
                 {
                     Log.d("Calc", e.getMessage());
@@ -164,19 +270,40 @@ public class SimpleActivity extends AppCompatActivity
     }
     private boolean getResult()
     {
+        boolean changeSign = false;
+
+        //CASE: Equation not empty
         if(currentOperator.equals("")) return false;
-        String[] operation = display.split(Pattern.quote(currentOperator));
+
+        //CASE: first number is negative
+        if(currentOperator.equals("-") && display.charAt(0) == '-')
+        {
+            display = display.substring(1);
+            changeSign = true;
+        }
+
+        //CASE: There are two numbers
+        String[] operation;
+
+        operation = display.replaceAll("\\s+", "").split(Pattern.quote(currentOperator));
+
         if(operation.length<2) return false;
 
-
-        result = ""+operate(operation[0], operation[1], currentOperator);
+        if(changeSign)
+        {
+            display = "-" + display;
+            result = "" + operate("-" + operation[0], operation[1], currentOperator);
+        }
+        else
+            result = ""+operate(operation[0], operation[1], currentOperator);
         return true;
     }
     @SuppressLint("SetTextI18n")
     public void onClickEqual(View v)
     {
+        //TODO: Split display to two TextViews and change color for result operation
         if(display.equals("")) return;
         if(!getResult()) return;
-        screen.setText(display + "\n" + String.valueOf(result));
+        screen.setText(display + "\n= " + String.valueOf(result));
     }
 }
